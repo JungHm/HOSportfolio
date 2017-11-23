87034c46f20e834da70a086f0d2c9cfe7b741405
fpk666 <gkwhdtn666@gmail.com>
2017-11-23 16:07:48 +0900

    merge 오류 제거

--- 4Dollor/D3DX_Base/cParticleManager.h
+++ 4Dollor/D3DX_Base/cParticleManager.h
@@ -16,12 +16,9 @@ public:
 	cParticleManager();
 	~cParticleManager();
 
-<<<<<<< HEAD
-	void AddParticle();
-=======
+
 	void AddHitParticle();
 	
 
->>>>>>> 1a2ef2cc514bb9e961f0e1f2d796ac3fe9e47d84
 };
 
