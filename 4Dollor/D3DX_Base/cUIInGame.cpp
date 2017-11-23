69195e51dfc1789f43408ef302ac5b8b109ee130
fpk666 <gkwhdtn666@gmail.com>
2017-11-23 16:00:47 +0900

    영웅 HP bar 추가

--- 4Dollor/D3DX_Base/cUIInGame.cpp
+++ 4Dollor/D3DX_Base/cUIInGame.cpp
@@ -4,6 +4,9 @@
 
 cUIInGame::cUIInGame()
 	: m_GameEnd(false)
+	, m_HPBarSizeX(6.0f)
+	, m_HPBarSizeY(0.5f)
+	, m_HPBarHeight(18)
 {
 }
 
@@ -118,12 +121,121 @@ void cUIInGame::SetSkillUseCooldown(int SkillIndex, float count)
 
 void cUIInGame::setupOther()
 {
+	setupHpBar(L"UI/ingame_img_bar_hp_bg.png");
+	setupHpBar(L"UI/ingame_img_bar_hp.dds");
 }
 
 void cUIInGame::updateOther()
 {
+
 }
 
 void cUIInGame::renderOther()
 {
+	renderBar();
+}
+
+void cUIInGame::setupHpBar(wstring filePath)
+{
+	tagHPBar hb;
+	ZeroMemory(&hb, sizeof(tagHPBar));
+	m_VHPBar.push_back(hb);
+	tagHPBar *phb = &m_VHPBar[m_VHPBar.size() - 1];
+
+	phb->max = HPMAX;
+	phb->current = HPMAX;
+
+	D3DXMatrixIdentity(&phb->matWorld);
+	D3DXMATRIXA16 matT, matR;
+	D3DXMatrixIdentity(&matT);
+	D3DXMatrixIdentity(&matR);
+
+	D3DXMatrixRotationX(&matR, -D3DX_PI / 2);
+	D3DXMatrixTranslation(&matT, -m_HPBarSizeX, m_HPBarHeight, 0.0f);
+	phb->matWorld = matR * matT;
+	
+	
+	phb->filePath = filePath;
+	D3DXVECTOR2 v2(0, 0);
+	g_pTextureManager->AddTexture(phb->filePath.c_str(), phb->texture, &v2);
+	
+	ST_PT_VERTEXT p;
+	p.p = D3DXVECTOR3(0, 0, -m_HPBarSizeY);	// 사이즈
+	p.t = D3DXVECTOR2(0, 1);		// 텍스쳐
+	phb->vertex.push_back(p);
+	p.p = D3DXVECTOR3(0, 0, m_HPBarSizeY);
+	p.t = D3DXVECTOR2(0, 0);
+	phb->vertex.push_back(p);
+	p.p = D3DXVECTOR3(m_HPBarSizeX * 2, 0, m_HPBarSizeY);
+	p.t = D3DXVECTOR2(1, 0);
+	phb->vertex.push_back(p);
+
+	p.p = D3DXVECTOR3(0, 0, -m_HPBarSizeY);
+	p.t = D3DXVECTOR2(0, 1);
+	phb->vertex.push_back(p);
+	p.p = D3DXVECTOR3(m_HPBarSizeX * 2, 0, m_HPBarSizeY);
+	p.t = D3DXVECTOR2(1, 0);
+	phb->vertex.push_back(p);
+	p.p = D3DXVECTOR3(m_HPBarSizeX * 2, 0, -m_HPBarSizeY);
+	p.t = D3DXVECTOR2(1, 1);
+	phb->vertex.push_back(p);
+}
+
+void cUIInGame::updateBar(bool pc, D3DXVECTOR3 pt, int currHp)
+{
+	// 체력의 비율을 계산해서 scale.x 값을 조정.
+	if (currHp < 0) currHp = 0;
+	else if (currHp > m_VHPBar[1].max) currHp = HPMAX;
+
+	m_VHPBar[1].current = currHp;
+	float scale = m_VHPBar[1].current / m_VHPBar[1].max;
+	if (scale < 0) scale = 0;
+	else if (scale > 1) scale = 1;
+
+	D3DXMatrixIdentity(&m_VHPBar[1].matWorld);
+	D3DXMATRIXA16 matT, matR, matS;
+	D3DXMatrixIdentity(&matT);
+	D3DXMatrixIdentity(&matR);
+	D3DXMatrixIdentity(&matS);
+
+	D3DXMatrixScaling(&matS, (m_VHPBar[1].current / m_VHPBar[1].max), 1, 1);
+	D3DXMatrixRotationX(&matR, -D3DX_PI / 2);
+	D3DXMatrixTranslation(&matT, -m_HPBarSizeX + pt.x, m_HPBarHeight + pt.y, pt.z);
+
+	for (int i = 0; i < m_VHPBar.size(); i++)
+	{
+		m_VHPBar[i].matWorld = matR * matT;
+	}
+
+	if (pc)
+	{
+		m_VHPBar[1].matWorld = matS * matR * matT;
+	}
+	else if (!pc)
+	{
+		m_VHPBar[3].matWorld = matS * matR * matT;
+	}
+}
+
+void cUIInGame::renderBar()
+{
+	
+	for (int i = 0; i < m_VHPBar.size(); i++)
+	{
+		g_pD3DDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
+		g_pD3DDevice->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
+		g_pD3DDevice->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_DIFFUSE);
+
+		g_pD3DDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
+		g_pD3DDevice->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+		g_pD3DDevice->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+
+		g_pD3DDevice->SetRenderState(D3DRS_TEXTUREFACTOR, D3DCOLOR_ARGB(255, 255, 255, 255));
+
+		g_pD3DDevice->SetFVF(ST_PT_VERTEXT::FVF);
+		g_pD3DDevice->SetTransform(D3DTS_WORLD, &m_VHPBar[i].matWorld);
+		g_pD3DDevice->SetTexture(0, m_VHPBar[i].texture);
+		g_pD3DDevice->DrawPrimitiveUP(D3DPT_TRIANGLELIST, m_VHPBar[i].vertex.size() / 3, &m_VHPBar[i].vertex[0], sizeof(ST_PT_VERTEXT));
+		g_pD3DDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+	}
 }
--- 4Dollor/D3DX_Base/cUIInGame.h
+++ 4Dollor/D3DX_Base/cUIInGame.h
@@ -3,6 +3,18 @@
 
 #define SKILLCOUNT 5
 #define SKILLCOUNTNUM 10
+#define HPMAX 100
+
+struct tagHPBar
+{
+	float current;
+	float max;
+	wstring filePath;
+	LPDIRECT3DTEXTURE9 texture;
+	D3DXMATRIXA16 matWorld;
+	D3DXVECTOR3 pos;
+	vector<ST_PT_VERTEXT> vertex;
+};
 
 class cUIInGame : public cUIObject
 {
@@ -10,12 +22,17 @@ private:
 	// 현재 버튼 클릭으로 씬 넘길 때 사용함.
 	SYNTHESIZE(bool, m_GameEnd, GameEnd);
 
+	vector<tagHPBar> m_VHPBar;
+	// 미니언은 map으로 만들고 첫번째 인자에 걔들 클래스? 주소값을 넣자
+	float m_HPBarSizeX;
+	float m_HPBarSizeY;
+	float m_HPBarHeight;
+
 public:
 	cUIInGame();
 	~cUIInGame();
 
-	int updateButtonOverCallback(int num);
-	int updateButtonCallback(int num);
+	
 
 	// 스킬 언락. q = 1, w = 2 순서
 	void SetSkillUnlock(int SkillIndex, bool unlock);
@@ -24,8 +41,18 @@ public:
 	// 스킬 사용 쿨타임 표시
 	void SetSkillUseCooldown(int SkillIndex, float count);
 
+	// 순수 가상함수로 실행하는 것들
 	void setupOther();
 	void updateOther();
 	void renderOther();
+	int updateButtonOverCallback(int num);
+	int updateButtonCallback(int num);
+
+	// 체력바 셋팅
+	void setupHpBar(wstring filePath);
+	// 체력 비율, matrix scale 계산
+	void updateBar(bool pc, D3DXVECTOR3 pt, int currHp);
+	// 체력바 랜더
+	void renderBar();
 };
 
